b0dacf6c2862eae26e80409e83649b976660e495
mayurisonawan2001 <75942262+mayurisonawan2001@users.noreply.github.com>
2025-05-29 09:05:51 +0530

    UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,6 @@
+package com.youreventease.repository;
+
+import com.youreventease.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
